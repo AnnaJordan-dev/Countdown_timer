df6a7cfbc549efb3f3b783a0ac40dc0642ab11c2
Anna <annalj41@gmail.com>
2023-09-03 14:35:11 +0100

    Initial Commit

---
+++ coundown_timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  coundown_timer
+//
+//  Created by Anna Jordan on 03/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("This is a test!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coundown_timer/coundown_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  coundown_timerApp.swift
+//  coundown_timer
+//
+//  Created by Anna Jordan on 03/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct coundown_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
